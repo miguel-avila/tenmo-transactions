84dd32230473c75d504d74af7924a6f9c5a7aaba
Elena Zelenina <zelenina.elena@gmail.com>
2021-02-05 18:44:40 -0500

    Step 3 works again!

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferSqlDAO.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/TransferSqlDAO.java
@@ -13,6 +13,9 @@ import com.techelevator.tenmo.model.Transfer;
 @Component
 public class TransferSqlDAO implements TransferDAO {
 
+	private static final int TRANSFER_TYPE_SEND = 2;
+	private static final int TRANSFER_STATUS_APPROVED = 2;
+	
 	private JdbcTemplate jdbcTemplate;
 	
 	public TransferSqlDAO(JdbcTemplate jdbcTemplate) {
@@ -36,14 +39,13 @@ public class TransferSqlDAO implements TransferDAO {
 	//Step 6. Retrieves the details of a transfer based upon the transfer ID
 	@Override
 	public Transfer getTransferById(int id) {
+		Transfer transfer = null;
 		String sql = "SELECT * FROM transfers WHERE transfer_id = ?";
 		SqlRowSet result = jdbcTemplate.queryForRowSet(sql, id);
 		if(result.next()) {
-			Transfer transfer = mapRowToTransfer(result);
-			return transfer;
-		} else {
-			throw new UsernameNotFoundException("");
+			transfer = mapRowToTransfer(result);
 		}
+		return transfer;
 	}
 
 	//Step 4.2 Creates a transfer including User IDs and the amount of TE Bucks.
@@ -51,22 +53,40 @@ public class TransferSqlDAO implements TransferDAO {
 	public boolean create(int account_from, int account_to, double amount) {
 		boolean transferCreated = false;
 		String insertTransfer = "INSERT INTO transfers (transfer_type_id, "
-				+ "transfer_status_id, account_from, account_to, amount) VALUES (2, 2, ?, ?, ?)";
+				+ "transfer_status_id, account_from, account_to, amount) VALUES "
+				+ "(" + TRANSFER_TYPE_SEND + ", " + TRANSFER_STATUS_APPROVED + ", ?, ?, ?)";
 		transferCreated = jdbcTemplate.update(insertTransfer, account_from, account_to, amount) == 1;
 		
 		return transferCreated;
 	}
 
+	//Step 4.3 Increases receiver's account balance by the amount of the transfer
 	@Override
 	public boolean increaseBalance(int account_to, double amount) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean balanceIncreased = false;
+		String addMoney = "UPDATE accounts SET balance = balance + ? WHERE account_id = ?";
+		balanceIncreased = jdbcTemplate.update(addMoney, amount, account_to) == 1;
+		
+		return balanceIncreased;
 	}
 
+	//Step 4.4 Decreases sender's account balance by the amount of the transfer
 	@Override
 	public boolean decreaseBalance(int account_from, double amount) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean balanceDecreased = false;
+		String checkBalance = "SELECT balance FROM accounts WHERE account_id = ?";
+		SqlRowSet result = jdbcTemplate.queryForRowSet(checkBalance, account_from, double.class);
+		double balance = 0.0;
+		if(result.next()) {
+			balance = result.getDouble("balance");
+		}
+		String substractMoney = "UPDATE accounts SET balance = ? WHERE account_id = ?";
+		if(balance >= amount) {
+			balance -= amount;
+			balanceDecreased = jdbcTemplate.update(substractMoney, balance, account_from) == 1;
+		}
+		
+		return balanceDecreased;
 	}
 
 	//helper method
